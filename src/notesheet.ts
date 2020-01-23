b495da6c36562d1b023e1bae1e1f42ce69595511
halvardssm <halvard@simplyundoable.com>
2020-01-23 19:04:59 +0100

    notesheet

--- src/notesheet.ts
+++ src/notesheet.ts
@@ -1,3 +1,59 @@
-class Notesheet {
+import Note from './Note'
 
+export default class Notesheet {
+  notes: Note[];
+  ctx: CanvasRenderingContext2D;
+
+  constructor(notes: Note[]) {
+  	this.notes = notes
+  }
+
+  updateNotes() {
+  	notes.forEach(element => {
+  		ctx.beginPath()
+  		ctx.lineWidth = '6'
+
+  		switch (element.finger) {
+  			case 0:
+  				ctx.strokeStyle = 'yellow'
+  				break
+
+  			case 1:
+  				ctx.strokeStyle = 'blue'
+  				break
+
+  			case 2:
+  				ctx.strokeStyle = 'green'
+  				break
+
+  			case 3:
+  				ctx.strokeStyle = 'red'
+  				break
+
+  			default:
+  				break
+  		}
+
+  		ctx.fillRect(element.posX, 50 + element.string * 40, element.duration, 10)
+  		ctx.stroke()
+  	})
+  }
+
+  startNotes(speed) {
+  	if (typeof speed == 'number' && speed >= 1 && loop == undefined) {
+  		loop = setInterval(update, speed)
+  		return true
+  	}
+  	return false
+  }
+
+  stopNotes() {
+  	if (loop != undefined) {
+  		clearInterval(loop)
+  		loop = undefined
+  		return true
+  	} else {
+  		return false
+  	}
+  }
 }
